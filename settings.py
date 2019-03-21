3c7f731bf17751fabc078ce35713b37570e79724
Niqui O'Neill <doneill>
2019-03-21 13:51:36 -0400

    update token

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 filepath = "_annotations"
-github_token = "e4dcdc87522b4ff87209c49e9ac5b8750d6c5459"
+github_token = "fa9a781f8b99a7321465d0bbcee00a73bd1a17c0"
 github_username = "dnoneill"
 github_repo = "annotate"
 github_url = "https://api.github.com/repos/{}/{}/contents".format(github_username, github_repo)
